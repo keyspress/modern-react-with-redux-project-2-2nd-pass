ee249cdfee9ad3536705187dc7e8722a1c68500d
Kyle Desktop <kylespresser@gmail.com>
2018-05-24 11:30:52 -0400

    lecture 50

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    { title: 'javascript: the good parts' },
-    { title: 'harry potter' },
-    { title: 'the dark tower' },
-    { title: 'eloquent ruby' }
+    { title: 'javascript: the good parts', pages: 101 },
+    { title: 'harry potter', pages: 39 },
+    { title: 'the dark tower', pages: 85 },
+    { title: 'eloquent ruby', pages: 1 }
   ]
 }
